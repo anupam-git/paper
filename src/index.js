c75c1f41be4486c0b64341e045459ad2801f7009
Anupam Basak <anupam.basak27@gmail.com>
2018-05-07 15:29:54 +0530

    Remove OLD Code

--- src/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.RandomWallpaper = require(__dirname+"/randomWallpaper");
-module.exports.WallpaperSetter = require(__dirname+"/wallpaperSetter");
-module.exports.SettingsManager = require(__dirname+"/settingsManager");
--- src/wallpaperSetter/index.js
+++
@@ -1,7 +0,0 @@
-const wallpaper = require('wallpaper');
- 
-function set(filepath) {
-    return wallpaper.set(filepath);
-}
-
-module.exports.set = set;
