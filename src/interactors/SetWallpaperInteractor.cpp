d5145dfccb00079ab18723dcd98ef1299c7e0c98
Anupam Basak <anupam.basak27@gmail.com>
2018-06-22 17:08:31 +0530

    Fix : --download flag enabled, still setting wallpaper

--- src/interactors/SetWallpaperInteractor.cpp
+++ src/interactors/SetWallpaperInteractor.cpp
@@ -18,26 +18,26 @@ void SetWallpaperInteractor::execute() {
 
   if (this->configHelper->get(ConfigEnum::DOWNLOAD) == "true") {
     this->commonUtils->say("  - Download flag enabled. Not Setting Wallpaper");
-  }
+  } else {
+    while (iterator.hasNext()) {
+      iterator.next();
 
-  while (iterator.hasNext()) {
-    iterator.next();
+      if (this->shellHelper->testCommand(iterator.key())) {
+        QString cmd = iterator.value();
 
-    if (this->shellHelper->testCommand(iterator.key())) {
-      QString cmd = iterator.value();
+        cmd.replace(":filepath",
+                    this->configHelper->get(ConfigEnum::DIR) + "/" +
+                        this->configHelper->get(ConfigEnum::WALLPAPER));
+        int returnCode = this->shellHelper->runCommand(cmd);
 
-      cmd.replace(":filepath",
-                  this->configHelper->get(ConfigEnum::DIR) + "/" +
-                      this->configHelper->get(ConfigEnum::WALLPAPER));
-      int returnCode = this->shellHelper->runCommand(cmd);
+        if (returnCode > 0) {
+          this->commonUtils->say("  - Error Setting Wallpaper");
+        } else {
+          this->commonUtils->say("  - Wallpaper Set");
+        }
 
-      if (returnCode > 0) {
-        this->commonUtils->say("  - Error Setting Wallpaper");
-      } else {
-        this->commonUtils->say("  - Wallpaper Set");
+        break;
       }
-
-      break;
     }
   }
 
